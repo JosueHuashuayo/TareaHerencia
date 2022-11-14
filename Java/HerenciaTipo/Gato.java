a4edca3980c1ac748efded874f9d8397657b220b
JosueHuashuayo <huashuayojosue@gmail.com>
2022-11-13 22:20:41 -0500

    Ejemplo de Herencia de tipo en java

---
+++ Java/HerenciaTipo/Gato.java
@@ -0,0 +1,7 @@
+package Java.HerenciaTipo;
+
+public class Gato implements Animal{
+    public void comunicar(){
+        System.out.println("Miauu!!!");
+    }
+}
---
+++ Java/HerenciaTipo/Perro.java
@@ -0,0 +1,7 @@
+package Java.HerenciaTipo;
+
+public class Perro implements Animal{
+    public void comunicar(){
+        System.out.println("Guau!!!");
+    }
+}
